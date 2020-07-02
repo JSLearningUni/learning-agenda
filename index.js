402adfb04e496c177838b9d9d623fb151ad4ec8b
leandro-miguel <miguel.apmbb@gmail.com>
2020-07-02 01:17:24 -0300

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cool = require('cool-ascii-faces');
 const express = require('express')
 const path = require('path')
-const { Pool } = require('pg');
+const { Pool } = require('pg'); "postgree"
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
